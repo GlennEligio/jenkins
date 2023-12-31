3955d7c4d7926f92f6391584bf147355a7df18dc
John Glenn Eligio <glenneligio@gmail.com>
2023-12-31 23:35:02 +0800

    Initial commit

---
+++ fe-deploy.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent any
+    
+    parameters {
+        gitParameter branchFilter: 'origin/(.*)', defaultValue: 'master', name: 'BRANCH', type: 'PT_BRANCH', quickFilterEnabled: true, sortMode: "DESCENDING_SMART"
+    }
+    
+    stages {
+        stage ('Fetch source code') {
+            steps {
+                sh 'cd /var/lib/jenkins/workspace/dntx/deploy/fe-deploy'
+                git branch: "${params.BRANCH}", url: 'https://github.com/GlennEligio/dn-tx.git'
+            }
+        }
+        
+        stage ('Install dependencies') {
+            steps {
+                sh 'cd /var/lib/jenkins/workspace/dntx/deploy/fe-deploy/frontend && npm install'
+            }
+        }
+        
+        stage ('Build docker image') {
+			steps {
+				sh "cd ./frontend && docker build -f Dockerfile.prod -t 'shuntjg/dntx-frontend:${params.BRANCH}_${BUILD_TIMESTAMP}' ."
+			}
+        }
+        
+        stage('Docker Image') {
+            steps {
+                script {
+                    // Use withDockerRegistry to push the image
+                    withDockerRegistry([url: "", credentialsId: "docker-hub-credentials"]) {
+                        sh "docker push 'shuntjg/dntx-frontend:${params.BRANCH}_${BUILD_TIMESTAMP}'"
+                    }
+                }
+            }
+        }
+    }
+}
